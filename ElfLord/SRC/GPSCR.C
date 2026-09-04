dcb86394f9f4121000154d0ad80fb613aac9ef10
Eric Hooks <ericbhooks@outlook.com>
2026-09-04 12:38:30 -0500

    Added a GamePlayScreen for main game play.

---
+++ ElfLord/SRC/GPSCR.C
@@ -0,0 +1,32 @@
+/**
+ * Gpscr.c
+ * Author: Eric Hooks, 2026
+ * Purpose: To implement the game play screen.
+ */
+
+ #include "HEADER/GPSCR.H"
+ #include "HEADER/SCREEN.H"
+ #include "HEADER/GAME.H"
+ #include "HEADER/PLAYER.H"
+ #include <GRX20.H>
+
+/**
+ * Function to render the game play screen.
+ */
+void renderGameplayScreen(Screen* s, Game* g) {
+
+}
+
+/**
+ * Function to render the game play area.
+ */
+void renderGameplayArea(Screen* s, Game* g) {
+
+}
+
+/**
+ * Function to render the hud area.
+ */
+void renderHudArea(Screen* s, Game* g) {
+
+}
